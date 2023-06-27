664bedc7d376497b609fd56d3bbbc49c7d9a41a5
Mangut <innocentmangut@gmail.com>
2023-06-27 15:44:42 +0100

    working request. remaining the cli part

--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -60,13 +60,13 @@ class MySqliteRequest
     def update(table_name)
         @request = 'UPDATE'
         @table_name = table_name
+        return self
 
     end
 
     def set(data)
         @data = data
         return self
-
     end
 
     def delete
@@ -75,7 +75,7 @@ class MySqliteRequest
 
     end
 
-
+    # method to print the result of query execution
     def print_result(result)
         if !result
             return
@@ -85,9 +85,11 @@ class MySqliteRequest
             return
         else
             puts "result: \n"
+            # peints the headers of the hash given
             puts result.first.keys.join(' | ')
             puts '-' * result.first.keys.join(' | ').length
             result.each do |row|
+                # prints the values of the hash 
                 puts row.values.join(' | ')
             end
             puts '-' * result.first.keys.join(' | ').length
@@ -108,34 +110,40 @@ class MySqliteRequest
         when 'SELECT'
             result = []
             if @join != nil
+                # opens both csv files 
                 csv_b = CSV.read(@table_name_to_join, headers: true).map(&:to_h)
                 csv_a = csv_hash
+                # to store the final join result
                 join_records = []
                 csv_a.each do |record_a|
                     csv_b.each do |record_b|
+                        # merges the selected records into one file joined_records
                         if record_a[@join[:column_a].to_s] == record_b[@join[:column_b].to_s]
                             join_records << record_a.merge(record_b)
                         end
                     end
                 end
+                # updates the hash result to include only the joined result
                 csv_hash = join_records
 
             end
             if @order != nil
+                # orders the result of the querry
                 csv_hash = csv_hash.sort_by {|record| record[@order[:column_name].to_s]}
                 csv_hash.reverse! if @order[:order] == 'desc'
             end
 
             if @where != nil
+                # implements where to select only selected records
                 csv_hash = csv_hash.select do |record|
                     record[@where[:column].to_s] == @where[:value]
                 end
             end
-
+            # final block that performs the select and prints the result using print_result function
             if @columns != nil && @table_name != nil 
                 csv_hash.each do |record|
                     res = {}
-                    
+                    # slits the string given as the colum into array for easier fetching
                     @columns.split(',').map(&:strip).each do |column|
                         value = record[column]
                         res[column] = value
@@ -150,6 +158,7 @@ class MySqliteRequest
             end
         when 'INSERT'
             if @data != nil && @table_name != nil
+                # opens and append records to the csv file using csv_hash
                 csv_hash = CSV.open(@table_name, 'a') do |csv|
                     @data.each do |row|
                         csv << row.values
@@ -157,21 +166,35 @@ class MySqliteRequest
                 end
             end
             print_result(csv_hash)
-            
+       
         when 'UPDATE'
-            if @where != nil 
-                @where = {@where[:column_name] => @where[:criteria]}
+            if @where != nil
+                # gets the record using the where clause
+                csv_hash.select do |record|
+                    if record[@where[:column].to_s] == @where[:value]
+                        record.merge!(@data)
+                    end
+                end
+                # writes the updated record to the csv file
+                CSV.open(@table_name, 'w', write_headers: true, headers: csv_hash.first.keys) do |csv|
+                    csv_hash.each {|row| csv << row.values }
+                end
             end
-            parsed_file = update_operation(parsed_file, @where, @data)
-            write_to_file(parsed_file, @table_name)
+            
         when 'DELETE'
-            if @where != nil 
-                @where = {@where[:column_name] => @where[:criteria]}
+            if @where != nil
+                # deletes a record from the hash file
+                csv_hash.reject! do |record|
+                    record[@where[:column].to_s] == @where[:value]            
+                    
+                end
+                # writes the result to the csv file
+                CSV.open(@table_name, 'w', write_headers: true, headers: csv_hash.first.keys) do |csv|
+                    csv_hash.each {|row| csv << row.values }
+                end
             end
-            parsed_file = delete_operation(parsed_file, @where)
-            write_to_file(parsed_file, @table_name)
         end
-
+        # set all variables to nill 
         @request = nil
         @where = nil
         @table_name = nil
@@ -181,16 +204,28 @@ class MySqliteRequest
 
 end
 
+# code testing
+
 request = MySqliteRequest.new
-request = request.from('nba_player_data.csv')
-request = request.select('name, birth_date, position, college')
+request = request.from('nba_players.csv')
+request = request.select('Player')
+request = request.where('birth_state', 'Indiana')
+request.run
+# request = request.from('nba_player_data.csv')
+# request = request.select('name, birth_date, position, college')
+# request = request.where('college', 'Jos University')
 # request = request.join('college', 'nba_players.csv', 'college')
-request = request.order('asc', 'name')
+# request = request.order('asc', 'name')
 # request = request.insert('nba_player_data.csv')
 # request = request.values([
 #     {'name' => 'innocent Mangut', 'year_start' => '1990', 'year_end'=>'1999', 'position' => 'FF-10', 'height' => '3.56', 'weight' => '300', 'birth_date'=> 'June 24, 1968', 'college'=>'Jos University'},
 #     {'name' => 'innocent Silas', 'year_start' => '1990', 'year_end'=>'1999', 'position' => 'FF-10', 'height' => '3.56', 'weight' => '300', 'birth_date'=> 'June 24, 1968', 'college'=>'Jos University'}
 # ])
 
+# request = request.update('nba_player_data.csv')
+# request.from('nba_player_data.csv')
+# request = request.where('name', 'Ante Zizic')
+# request = request.delete()
+# request = request.set({'name' => 'Mangut Innocent', 'year_start' => '1890', 'year_end'=>'1999', 'position' => 'FF-10', 'height' => '3.56', 'weight' => '300', 'birth_date'=> 'June 24, 1968', 'college'=>'Jos University'})
 
-request.run
+
